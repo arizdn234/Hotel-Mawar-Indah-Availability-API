fe01088662f7b2b4af9f77316a4fd6eddc04ec03
Arizidane <samionohari@gmail.com>
2024-05-11 04:57:11 +0700

    add: room availability check repo dll

--- internal/repository/room_repository.go
+++ internal/repository/room_repository.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/arizdn234/hotel-mawar-indah-availability-api/internal/model"
 	"gorm.io/gorm"
 )
@@ -51,6 +53,52 @@ func (r *RoomRepository) UpdateRoom(room *model.Room) error {
 
 func (r *RoomRepository) DeleteRoom(id uint) error {
 	if err := r.DB.Delete(&model.Room{}, id).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *RoomRepository) GetAvailableRooms() ([]model.Room, error) {
+	var rooms []model.Room
+	if err := r.DB.Where("available = ?", true).Find(&rooms).Error; err != nil {
+		return nil, err
+	}
+
+	return rooms, nil
+}
+
+func (r *RoomRepository) CancelReservation(id uint) error {
+	var reservation model.Reservation
+	if err := r.DB.First(&reservation, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return errors.New("reservation not found")
+		}
+		return err
+	}
+	reservation.Status = "canceled"
+	if err := r.DB.Save(&reservation).Error; err != nil {
+		return err
+	}
+
+	if err := r.Available(reservation.RoomID, true); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *RoomRepository) Available(roomID uint, available bool) error {
+	var room model.Room
+	if err := r.DB.First(&room, roomID).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return errors.New("room not found")
+		}
+		return err
+	}
+
+	room.Available = available
+	if err := r.DB.Save(&room).Error; err != nil {
 		return err
 	}
 
