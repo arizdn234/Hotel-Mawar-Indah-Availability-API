b82ef6dc78826088c5830cfde51ee2049c649c38
Arizidane <samionohari@gmail.com>
2024-05-11 03:55:55 +0700

    fix: database configuration via env file

--- db/postgres.go
+++ db/postgres.go
@@ -1,6 +1,10 @@
 package db
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -8,7 +12,17 @@ import (
 var DB *gorm.DB
 
 func InitDB() error {
-	dsn := "host=localhost user=postgres password=postgres dbname=hotel-mawar-indah port=5432 sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	dsn := "host=" + os.Getenv("DB_HOST") +
+		" user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASSWORD") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" port=" + os.Getenv("DB_PORT") +
+		" sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return err
