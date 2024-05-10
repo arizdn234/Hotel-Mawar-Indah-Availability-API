1305686ba3a636cfb200c0783860bf499f48d57b
Arizidane <samionohari@gmail.com>
2024-05-11 03:56:35 +0700

    fix: port via env file

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/arizdn234/hotel-mawar-indah-availability-api/db"
 	"github.com/arizdn234/hotel-mawar-indah-availability-api/internal/api"
 	"github.com/arizdn234/hotel-mawar-indah-availability-api/internal/repository"
 	"github.com/arizdn234/hotel-mawar-indah-availability-api/internal/service"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Failed to load .env file: %v", err)
+	}
+
 	if err := db.InitDB(); err != nil {
 		log.Fatalf("Failed to initialize database: %v", err)
 	}
@@ -38,5 +45,10 @@ func main() {
 	// r.POST("/reservation", RoomReservation)
 	// r.DELETE("/reservation/:id/cancel", ReservationCancellation)
 
-	r.Run(":8080")
+	port := os.Getenv("PORT")
+
+	log.Printf("Server is running on port %v\n\n`http://localhost:%v`", port, port)
+	if err := r.Run(":" + port); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
