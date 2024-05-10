ab1418fcabfd16179392c285d0f6d2685ecf0402
Arizidane <samionohari@gmail.com>
2024-05-11 04:56:06 +0700

    add: core bussiness service

--- internal/service/room_service.go
+++ internal/service/room_service.go
@@ -1,12 +1,15 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/arizdn234/hotel-mawar-indah-availability-api/internal/model"
 	"github.com/arizdn234/hotel-mawar-indah-availability-api/internal/repository"
 )
 
 type RoomService struct {
-	RoomRepository *repository.RoomRepository
+	RoomRepository        *repository.RoomRepository
+	ReservationRepository *repository.ReservationRepository
 }
 
 func NewRoomService(roomRepository *repository.RoomRepository) *RoomService {
@@ -31,4 +34,45 @@ func (s *RoomService) UpdateRoom(room *model.Room) error {
 
 func (s *RoomService) DeleteRoom(id uint) error {
 	return s.RoomRepository.DeleteRoom(id)
+}
+
+func (s *RoomService) GetAvailableRooms() ([]model.Room, error) {
+	availableRooms, err := s.RoomRepository.GetAvailableRooms()
+	if err != nil {
+		return nil, err
+	}
+
+	return availableRooms, nil
+}
+
+func (s *RoomService) ReserveRoom(reservation *model.Reservation) error {
+	room, err := s.RoomRepository.GetRoomByID(reservation.RoomID)
+	if err != nil {
+		return err
+	}
+
+	if !room.Available {
+		return errors.New("room is not available")
+	}
+
+	room.Available = false
+	if err := s.RoomRepository.UpdateRoom(room); err != nil {
+		return err
+	}
+
+	if err := s.ReservationRepository.CreateReservation(reservation); err != nil {
+		room.Available = true
+		s.RoomRepository.UpdateRoom(room)
+		return err
+	}
+
+	return nil
+}
+
+func (s *RoomService) CancelReservation(id uint64) error {
+	err := s.RoomRepository.CancelReservation(uint(id))
+	if err != nil {
+		return err
+	}
+	return nil
 }
